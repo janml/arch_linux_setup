4d1ea917875a94f2f4490eb1a66900e70d8cca98
janml <jan-michael.loehlein@gmx.de>
2025-02-26 23:24:31 +0100

    Add bash script for setting my screenlayout.

---
+++ screenlayout/home.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Dell Inc. AW2725DF 74P4ZZ3
+swaymsg output DP-3 mode 2560x1440@140HZ pos 0 550
+
+# Dell Inc. DELL U2719D FFNWT13
+swaymsg output DP-5 mode 2560x1440@59HZ transform 270 pos 2560 0
+
+# Laptop
+swaymsg output eDP-1 mode 1920x1080@60HZ pos 4000 910
